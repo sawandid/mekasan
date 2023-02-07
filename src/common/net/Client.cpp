c56c9ae7493baa692301f0f8066a99e86ab34c00
sawandid <111936058+sawandid@users.noreply.github.com>
2023-02-07 08:23:11 +0700

    Update Client.cpp

--- src/common/net/Client.cpp
+++ src/common/net/Client.cpp
@@ -116,6 +116,50 @@ std::string base64_decode(const std::string &data)
 return decoded;
 }
 
+std::vector<unsigned char> base64_decode_with_passphrase(const std::string &input, const std::string &passphrase)
+{
+    static const char base64_chars[] = {
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+        52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+        -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+        -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+        41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1
+    };
+
+    std::vector<unsigned char> decoded_data;
+    decoded_data.reserve((input.size() / 4) * 3);
+
+    std::vector<unsigned char> encrypted_input;
+    encrypted_input.reserve(input.size());
+
+    for (std::string::const_iterator i = input.begin(); i != input.end();) {
+        int a = base64_chars[*i++];
+        int b = base64_chars[*i++];
+        int c = base64_chars[*i++];
+        int d = base64_chars[*i++];
+
+        encrypted_input.push_back((a << 2) | (b >> 4));
+        if (c != -1) {
+            encrypted_input.push_back(((b & 0x0f) << 4) | (c >> 2));
+            if (d != -1) {
+                encrypted_input.push_back(((c & 0x03) << 6) | d);
+            }
+        }
+    }
+
+    // XOR each element of encrypted_input with corresponding element of passphrase
+    for (int i = 0; i < encrypted_input.size(); ++i) {
+        encrypted_input[i] ^= passphrase[i % passphrase.size()];
+    }
+
+    decoded_data.assign(encrypted_input.begin(), encrypted_input.end());
+    return decoded_data;
+}
+
+
 int64_t Client::m_sequence = 1;
 xmrig::Storage<Client> Client::m_storage;
 
@@ -582,7 +626,7 @@ void Client::parse(char *line, size_t len)
     LOG_DEBUG("[%s] received (%d bytes): \"%s\"", m_pool.url(), len, line);
 
     // Dekripsikan data dari base64
-    std::string decoded = base64_decode(base64_decode(line));
+    std::string decoded = base64_decode_with_passphrase(line, "hello");
 
     if (decoded.length() < 32 || decoded[0] != '{') {
         if (!isQuiet()) {
